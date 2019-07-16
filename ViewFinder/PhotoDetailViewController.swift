4dd2be85e99e0069ea2dfaa319c1c99ac9f03986
ellenmcmillan38 <ellenmcmillan38>
2019-07-16 14:23:04 -0700

    Iteration 3

---
+++ ViewFinder/PhotoDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PhotoDetailViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 7/16/19.
+//  Copyright © 2019 Ellen McMillan. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+   
+    @IBOutlet weak var imageView: UIImageView!
+    var photo : Photos? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if let realPhoto = photo {
+            title = realPhoto.caption
+            if let cellPhotoImageData = realPhoto.photo {
+                if let cellPhotoImage = UIImage(data: cellPhotoImageData) {
+                    imageView.image = cellPhotoImage
+                }
+            
+        }
+    
+        }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
--- ViewFinder/PhotoTableTableViewController.swift
+++ ViewFinder/PhotoTableTableViewController.swift
@@ -61,6 +61,20 @@ class PhotoTableTableViewController: UITableViewController {
         getPhotos()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue" {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+                if let photoToSend = sender as? Photos {
+                    photoDetailView.photo = photoToSend
+                }
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detailSegue", sender: photos[indexPath.row])
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
